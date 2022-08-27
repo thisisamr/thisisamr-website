ba6d5dff9b2e097d5e79615db4b6cf85466ae627
Amr <thisissoliman@protonmail.com>
2022-08-27 20:49:04 +0200

    works page
    added /works page  to include the works section of the website

--- pages/work.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from 'next'
-
-const Works: NextPage = () => {
-  return <div>Work</div>
-}
-
-export default Works
--- pages/works.tsx
+++ pages/works.tsx
@@ -1,7 +1,14 @@
+import { Container, Heading } from '@chakra-ui/react'
 import { NextPage } from 'next'
 
 const Works: NextPage = () => {
-  return <div>Work</div>
+  return (
+    <Container mt={10}>
+      <Heading fontSize={20} mb={4}>
+        Works
+      </Heading>
+    </Container>
+  )
 }
 
 export default Works
