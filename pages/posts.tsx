3c1174cd3e131c7e60b462fbe48487b4e5bcb310
Asolyma <asolyma@hotmail.com>
2022-10-11 23:06:41 +0200

    added page posts

---
+++ pages/posts.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from '@chakra-ui/react'
+
+const posts = () => {
+  return (
+    <Box bg={'red.400'} mt="10">
+      <Text color={'white'}> There is nothing here yet!!!!!!!!!!!!!!!!</Text>
+    </Box>
+  )
+}
+
+export default posts
