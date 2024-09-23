e41a6ff488c325b5dd10294b6add91a5a407434d
Amr Soliman <thisissoliman@protonmail.com>
2024-09-23 14:01:46 +0300

    added issue-tracker and animePlanet

--- pages/works.tsx
+++ pages/works.tsx
@@ -5,32 +5,58 @@ import Section from '../components/Section'
 import Disney from '../public/works/disney.jpg'
 import Hulu from '../public/works/hulu_2.jpeg'
 import social from '../public/works/social-dash.png'
+// import trax from '../public/works/trax.png'
+import issue from '../public/works/issue-tracker.png'
 import tiny from '../public/works/tiny-trail-banner.jpg'
+import animeplanet from '../public/works/anime.png'
 let works = [
   {
     id: 'tiny-trail',
     title: 'Tiny Trail',
     thumb: tiny,
     description: 'A fast url shortener made with Go and Redis',
+    link: "https://github.com/thisisamr/tiny-trail"
   },
   {
     id: 'disney-clone',
     title: 'Desney plus+ clone',
     thumb: Disney,
     description: 'A clone of Disney plus+ website',
+    link: "https://disney-clone-iota.vercel.app/"
   },
   {
     id: 'Hulu-clone',
     title: 'Hulu clone',
     thumb: Hulu,
     description: 'A clone of Hulu website',
+    link: "http://hulu-clone-asolyma.vercel.app/"
   },
+  // {
+  //   id: 'trax',
+  //   title: 'Trax',
+  //   thumb: trax,
+  //   description:
+  //     'A modern music app inspired by Spotify and build with Nextjs and Prisma',
+  //   link: ""
+  // },
   {
     id: 'social-dash',
     title: 'Awesome-dash',
     thumb: social,
     description: 'A public wall made with nextjs and prisma.',
+    link: "https://social-dashboard-fullstack.vercel.app/"
   },
+  {
+    id: 'anime-planet',
+    title: "AnimePlanet",
+    thumb: animeplanet, description: 'Discover your favorite anime effortlessly with our sleek, infinite scrolling interface.',
+    link: 'https://anime-planet-opal.vercel.app/'
+  }
+  , {
+    id: "Issue-tracker",
+    title: "Issue Tracker",
+    thumb: issue, description: "An issue Tracker application build with Nextjs", link: "https://issue-tracker-web-delta.vercel.app/"
+  }
 ]
 const Works: NextPage = () => {
   return (
@@ -44,6 +70,7 @@ const Works: NextPage = () => {
                 id={project.id}
                 title={project.title}
                 thumbnail={project.thumb}
+                link={project.link}
               >
                 {project.description}
               </WorkGridItem>
